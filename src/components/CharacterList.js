1873c818f9d510522db3812b7f281afa9a4e5794
Antoni <ant.morawski@gmail.com>
2020-05-21 16:47:29 +0200

    displaying list with info

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 import { fetchCharacters } from "../actions"
+import "./CharacterList.css"
 
 class CharacterList extends Component {
   componentDidMount() {
     this.props.fetchCharacters()
   }
 
+  renderList() {
+    const { characters } = this.props
+
+    return characters.map((char) => {
+      return (
+        <div className="ui segment" key={char.id}>
+          <div className="item">
+            <img className="ui image" src={char.image} alt="character avatar" />
+            <div className="content">
+              <div className="description">
+                <h3>{char.name}</h3>
+                <p>Species: {char.species}</p>
+                <p>Status: {char.status}</p>
+                <p>Date created: {char.created}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
     console.log(this.props.characters)
-    return <div>CharList</div>
+    return <div className="ui relaxed divided list">{this.renderList()}</div>
   }
 }
 
